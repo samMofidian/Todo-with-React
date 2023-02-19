e0520f2e66ee943fa68cbb7188bf0cca95940d01
sAm <m.a.mofidian@hotmail.co.uk>
2023-02-19 22:11:32 +0330

    add clear all btn

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import { Plus } from "react-bootstrap-icons";
 export default function App() {
   return (
     <div className="app">
-      <h1>Todo App</h1>
-      <form className="todo-form">
-        <div className="todo-actions">
-          <input type="text" placeholder="Add your new todo" />
-          <button>
-            <Plus />
-          </button>
-        </div>
-      </form>
+      <div>
+        <h1>Todo App</h1>
+        <form className="todo-form">
+          <div className="todo-actions">
+            <input type="text" placeholder="Add your new todo" />
+            <button>
+              <Plus />
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="todo-footer">
+        <button>Clear All</button>
+      </div>
     </div>
   );
 }
