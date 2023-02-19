d9bd796f6220016e4bce6910f414a791b7fd9e9c
sAm <m.a.mofidian@hotmail.co.uk>
2023-02-19 22:01:51 +0330

    set title & form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Plus } from "react-bootstrap-icons";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <h1>Todo App</h1>
+      <form className="todo-form">
+        <div className="todo-actions">
+          <input type="text" placeholder="Add your new todo" />
+          <button>
+            <Plus />
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
