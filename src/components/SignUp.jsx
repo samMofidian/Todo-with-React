d2a11072cf3c296619813ffafd21e1022db09ff8
sAm <m.a.mofidian@hotmail.co.uk>
2023-02-26 22:38:13 +0330

    set sign-up form

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,54 @@
 import { useForm } from "react-hook-form";
+import "./SignUp.css";
 
 const SignUp = () => {
-  return <div>Sign Up</div>;
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+  const submitClickHandler = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="sign-up-container">
+      <form onSubmit={handleSubmit(onSubmit)} className="sign-up-form">
+        <label>
+          <p>First Name</p>
+          <input {...register("firstName")} type="text" />
+        </label>
+        <label>
+          <p>Last Name</p>
+          <input {...register("lastName")} type="text" />
+        </label>
+        <label>
+          <p>Provience</p>
+          <input {...register("provience")} type="text" />
+        </label>
+        <label>
+          <p>Phone Number</p>
+          <input {...register("phoneNumber")} type="phone" />
+        </label>
+        <label>
+          <p>Gender</p>
+          <select {...register("gender")}>
+            <option value="">Please choose one option:</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+        </label>
+        <label>
+          <p>ID</p>
+          <input {...register("id")} type="text" />
+        </label>
+        <label>
+          <p>Email</p>
+          <input {...register("email")} type="email" />
+        </label>
+        <div>
+          <input type="submit" onClick={submitClickHandler} />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
